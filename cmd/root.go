86bbbbf0825b35a9545fb256084cb06d4eb5e2c0
David Soane <david.soane@outlook.com>
2024-06-16 19:54:46 -0600

    added scripts and build CLI argument

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,20 +9,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "wsldwnl",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "WSL2 launcher and downloader",
+	Long:  `A WSL2 launcher and downloader for linux distros. Select from pre-configured distros or add your own using a rootfs tarball`,
 	// Uncomment the following line if your bare application
-	// has an action associated with it:	
+	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
@@ -47,5 +39,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
